e73ff1fb02f6234e87c6c3fefe188b2d2f204f28
antoniapiae <83601106+antoniapiae@users.noreply.github.com>
2021-11-20 09:30:00 +0100

    Added Bootstrap to the application

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Antonia Englert and is{" "}
-        <a
-          href="https://github.com/antoniapiae/react-weather-app-project"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Antonia Englert and is{" "}
+          <a
+            href="https://github.com/antoniapiae/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
