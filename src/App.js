25ebd7e8ef7cbf260976e77bbe17dd3803dde132
antoniapiae <83601106+antoniapiae@users.noreply.github.com>
2021-11-20 09:08:54 +0100

    Added GitHub repository

--- src/App.js
+++ src/App.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Antonia Englert and is{" "}
+        <a
+          href="https://github.com/antoniapiae/react-weather-app-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
